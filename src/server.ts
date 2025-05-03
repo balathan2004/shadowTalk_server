c84247109a0b1ab8a55c4f424255891ec82adf8c
light <balathan2004@gmail.com>
2025-05-03 19:51:53 +0530

    cloud test

--- src/server.ts
+++ src/server.ts
@@ -7,18 +7,20 @@ import cors from "cors";
 import { Server } from "socket.io";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
-const port =process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 app.use(cookieParser());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://192.168.143.57:5173",
+  "http://127.0.0.1:5173",
+  "http://192.168.133.57:5173",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://192.168.143.57:5173",
-      "http://127.0.0.1:5173",
-      "http://192.168.133.57:5173"
-    ],
+    origin: allowedOrigins,
     credentials: true,
   })
 );
@@ -32,23 +34,26 @@ app.use("/graphql", yoga);
 const server = createServer(app);
 
 export const io = new Server(server, {
-
   // ["http://localhost:5173","http://192.168.133.57:5173/"]
   cors: {
-    origin:"http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   },
 });
 
 export const users: Record<string, string> = {};
 
-export const getAllusers=()=>{
-  return users
-}
+export const getAllusers = () => {
+  return users;
+};
 
 export const getUserSocketId = (userId: string): string | undefined => {
   return users[userId]; // Return socket ID if user is online
 };
+
+app.get("/", (_, res) => {
+  res.send("Server is running");
+});
 
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId as string;
